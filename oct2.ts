5ba17fb246d5d8c47a6543141ed78918ceca0dcd
chandrak23 <124096082+chandrak23@users.noreply.github.com>
2024-10-03 07:14:45 -0500

    Update oct2.ts

--- oct2.ts
+++ oct2.ts
@@ -1,1 +1,8 @@
-test
+jest.mock('@grrs/server-shared-component_kafka', () => ({
+    kafkaClient: jest.fn().mockReturnValue({
+        brokers: ['localhost:9092'],
+    }),
+    kafkaProducer: jest.fn().mockReturnValue({
+        send: jest.fn(),
+    }),
+}));
